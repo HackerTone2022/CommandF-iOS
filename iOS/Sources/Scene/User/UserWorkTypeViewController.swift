020bbc6cfd7623b9e37c27c9f140fd3e60a3b400
sian7563 <sian7563@naver.com>
2022-11-04 01:36:21 +0900

    ⚡️ :: UserWorkTypeViewController UI 구현

---
+++ iOS/Sources/Scene/User/UserWorkTypeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserWorkTypeViewController.swift
+//  CommandF-iOS
+//
+//  Created by kimsian on 2022/11/04.
+//  Copyright © 2022 com.HackerTone2022. All rights reserved.
+//
+
+import UIKit
+
+class UserWorkTypeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
