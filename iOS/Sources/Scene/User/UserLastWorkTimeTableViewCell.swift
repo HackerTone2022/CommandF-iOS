94ea591cb60e2c614767227dd8c51b670b1e5687
sian7563 <sian7563@naver.com>
2022-11-04 06:38:39 +0900

    ⚡️: UserLastWrokTimeTableViewCell

---
+++ iOS/Sources/Scene/User/UserLastWorkTimeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserLastWorkTimeTableViewCell.swift
+//  CommandF-iOS
+//
+//  Created by kimsian on 2022/11/04.
+//  Copyright © 2022 com.HackerTone2022. All rights reserved.
+//
+
+import UIKit
+
+class UserLastWorkTimeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
