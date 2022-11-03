26df68427cf4ea81ab456642bee2fa966ffb4e4b
sian7563 <sian7563@naver.com>
2022-11-03 23:48:51 +0900

    ⚡️ :: UserAddToDoListViewController 구현

--- iOS/Sources/Scene/User/UserAddToDoListViewController.swift
+++ iOS/Sources/Scene/User/UserAddToDoListViewController.swift
@@ -10,11 +10,42 @@ class UserAddToDoListViewController: BaseViewController {
         $0.text = "ToDo"
     }
     private let toDoTextField = UITextField().then {
+        $0.backgroundColor = .white
         $0.layer.cornerRadius = 8
         $0.layer.borderWidth = 1
         $0.layer.borderColor = UIColor.setRGB(red: 255, green: 218, blue: 85, alpha: 100).cgColor
         $0.placeholder = "할 일 입력"
         $0.font = .systemFont(ofSize: 12, weight: .regular)
+        $0.addLeftPadding()
+    }
+    private let timeLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 14, weight: .medium)
+        $0.text = "Time"
+    }
+    private let timeTextField = UITextField().then {
+        $0.backgroundColor = .white
+        $0.layer.cornerRadius = 8
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = UIColor.setRGB(red: 255, green: 218, blue: 85, alpha: 100).cgColor
+        $0.placeholder = "시간 입력"
+        $0.font = .systemFont(ofSize: 12, weight: .regular)
+        $0.addLeftPadding()
+    }
+    private let contentLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 14, weight: .medium)
+        $0.text = "Content"
+    }
+    private let contentTextView = UITextView().then {
+        $0.font = .systemFont(ofSize: 12, weight: .regular)
+        $0.layer.cornerRadius = 8
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = UIColor.setRGB(red: 255, green: 218, blue: 85, alpha: 100).cgColor
+    }
+    private let addButton = UIButton(type: .system).then {
+        $0.backgroundColor = .setRGB(red: 255, green: 218, blue: 85, alpha: 100)
+        $0.setTitle("추가하기", for: .normal)
+        $0.setTitleColor(.black, for: .normal)
+        $0.layer.cornerRadius = 8
     }
 
     override func viewDidLoad() {
@@ -23,9 +54,14 @@ class UserAddToDoListViewController: BaseViewController {
     override func setNavigation() {
         self.navigationItem.title = "ToDo"
     }
-    override func addSubivews() {
+    override func addSubviews() {
         [toDoLabel,
-         toDoTextField
+         toDoTextField,
+         timeLabel,
+         timeTextField,
+         contentLabel,
+         contentTextView,
+         addButton
         ].forEach { view.addSubview($0) }
     }
     override func makeSubviewConstraints() {
@@ -37,6 +73,29 @@ class UserAddToDoListViewController: BaseViewController {
             $0.top.equalTo(toDoLabel.snp.bottom).offset(6)
             $0.leading.trailing.equalToSuperview().inset(15)
             $0.height.equalTo(48)
+        }
+        timeLabel.snp.makeConstraints {
+            $0.top.equalTo(toDoTextField.snp.bottom).offset(22)
+            $0.leading.equalToSuperview().inset(26)
+        }
+        timeTextField.snp.makeConstraints {
+            $0.top.equalTo(timeLabel.snp.bottom).offset(6)
+            $0.leading.trailing.equalToSuperview().inset(15)
+            $0.height.equalTo(48)
+        }
+        contentLabel.snp.makeConstraints {
+            $0.top.equalTo(timeTextField.snp.bottom).offset(22)
+            $0.leading.equalToSuperview().inset(26)
+        }
+        contentTextView.snp.makeConstraints {
+            $0.top.equalTo(contentLabel.snp.bottom).offset(6)
+            $0.leading.trailing.equalToSuperview().inset(15)
+            $0.height.equalTo(300)
+        }
+        addButton.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(15)
+            $0.bottom.equalToSuperview().inset(42)
+            $0.height.equalTo(50)
         }
     }
 }
