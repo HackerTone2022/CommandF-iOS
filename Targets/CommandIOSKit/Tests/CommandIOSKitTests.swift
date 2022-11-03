66935118b6edb58df1772f8da1320f582171fcbc
rlarldud1234 <rlarldud6482@naver.com>
2022-11-03 11:18:25 +0900

    ⚙️ :: 세팅

---
+++ Targets/CommandIOSKit/Tests/CommandIOSKitTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class CommandIOSKitTests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("CommandIOSKit", "CommandIOSKit")
+    }
+}
---
+++ Targets/CommandIOSUI/Tests/CommandIOSUITests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class CommandIOSUITests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("CommandIOSUI", "CommandIOSUI")
+    }
+}
