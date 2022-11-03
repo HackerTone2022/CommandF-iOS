c69894462dc7f6677d7c075623c97c52248fb01b
rlarldud1234 <rlarldud6482@naver.com>
2022-11-03 14:44:48 +0900

    ⚙️ :: 프로젝트 파일

---
+++ Library/Project.swift
@@ -0,0 +1,15 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.dynamicFramework(
+    name: "Library",
+    platform: .iOS,
+    dependencies: [
+        .SPM.RxSwift,
+        .SPM.RxCocoa,
+        .SPM.Moya,
+        .SPM.RxMoya,
+        .SPM.SnapKit,
+        .SPM.Then
+    ]
+)
